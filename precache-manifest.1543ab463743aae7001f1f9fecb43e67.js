98aaf5c492bff0a2f69a13a7c8c2cb76814fc8b2
Jeffrey-Wang <17jwapples@gmail.com>
2019-05-31 15:04:45 -0400

    Updates

---
+++ precache-manifest.1543ab463743aae7001f1f9fecb43e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaaa8fd68c5ebad2d02664b370bfb1a",
+    "url": "/judgementhall/index.html"
+  },
+  {
+    "revision": "6766a2c0b755e9f0781e",
+    "url": "/judgementhall/static/css/main.0e8bef65.chunk.css"
+  },
+  {
+    "revision": "0b116e9af185b9633e54",
+    "url": "/judgementhall/static/js/2.24f5194f.chunk.js"
+  },
+  {
+    "revision": "6766a2c0b755e9f0781e",
+    "url": "/judgementhall/static/js/main.5466f1cc.chunk.js"
+  },
+  {
+    "revision": "1b960854a02f5829ae45",
+    "url": "/judgementhall/static/js/runtime~main.ce401fe5.js"
+  }
+]);
